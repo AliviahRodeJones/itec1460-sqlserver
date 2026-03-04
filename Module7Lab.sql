2946ecd4c7a1659c9e885e2fed464665b2049cb1
AliviahRodeJones <ii3862au@go.minnstate.edu>
2026-03-04 01:47:24 +0000

    Completed Module 7 La Part 2

--- Module7Lab.sql
+++ Module7Lab.sql
@@ -132,3 +132,106 @@ EXEC CalculateOrderTotal
 
 PRINT ('Returned total: $' + CAST(@TotalAmount AS NVARCHAR(20)));
 GO
+
+-- Module 7 Part 2 LAB --
+
+USE Northwind;
+GO
+
+-- =============================================
+-- Part 2, Procedure 1: GetProductName
+-- =============================================
+
+CREATE OR ALTER PROCEDURE GetProductName
+    @ProductID INT,
+    @ProductName NVARCHAR(40) OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT @ProductName = ProductName
+    FROM Products
+    WHERE ProductID = @ProductID;
+
+    IF @ProductName IS NULL
+        PRINT 'Product not found.';
+    ELSE
+        PRINT 'Product Name: ' + @ProductName;
+END
+GO
+
+-- Test it
+DECLARE @ProductName NVARCHAR(40);
+
+EXEC GetProductName
+    @ProductID = 1,
+    @ProductName = @ProductName OUTPUT;
+GO
+
+
+-- =============================================
+-- Part 2, Procedure 2: GetEmployeeOrderCount
+-- =============================================
+
+CREATE OR ALTER PROCEDURE GetEmployeeOrderCount
+    @EmployeeID INT,
+    @OrderCount INT OUTPUT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT @OrderCount = COUNT(*) 
+    FROM Orders
+    WHERE EmployeeID = @EmployeeID
+
+    PRINT (@EmployeeID + CAST(@OrderCount AS NVARCHAR(10)));
+    END
+GO
+
+-- Test it
+DECLARE @OrderCount INT;
+
+EXEC GetEmployeeOrderCount
+    @EmployeeID = 5,
+    @OrderCount = @OrderCount OUTPUT;
+GO
+
+
+--Part 3: Check if a product needs reordering
+CREATE OR ALTER PROCEDURE CheckProductStock
+    @ProductID INT,
+    @NeedsReorder BIT Output
+AS 
+    BEGIN
+    SET NOCOUNT ON
+    
+    DECLARE @UnitsInStock NVARCHAR(10);
+    DECLARE @ReorderLevel NVARCHAR(10);
+
+    SELECT @UnitsInStock = UnitsInStock, @ReorderLevel = ReorderLevel
+    FROM Products
+    WHERE ProductID = @ProductID;
+
+    IF @UnitsInStock < @ReorderLevel 
+        BEGIN
+        SET @NeedsReorder = 1
+        PRINT ('Product ' + CAST(@ProductID AS NVARCHAR(10)) + ' needs reordering'); 
+        END
+    IF @UnitsInStock > @ReorderLevel 
+        BEGIN
+        SET @NeedsReorder = 0
+        PRINT ('Product ' + CAST(@ProductID AS NVARCHAR(10)) + ' stock is OK.');
+        END
+    END
+GO
+
+-- Test CheckProductStock
+DECLARE @NeedsReorder BIT;
+
+EXEC CheckProductStock
+    @ProductID = 2,
+    @NeedsReorder = @NeedsReorder OUTPUT;
+
+PRINT 'Needs Reorder flag: ' + CAST(@NeedsReorder AS VARCHAR(1));
+GO
+
