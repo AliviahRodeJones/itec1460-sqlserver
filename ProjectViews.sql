2e920a971fe8ff38e5e6c2bc9571f2d9d2fc03a2
AliviahRodeJones <ii3862au@go.minnstate.edu>
2026-05-07 16:13:09 +0000

    Finished Project Views

--- ProjectViews.sql
+++ ProjectViews.sql
@@ -1,1 +1,27 @@
 USE ProjectDatabase;
+
+CREATE VIEW VisitorView AS 
+SELECT
+    c.CatID, 
+    c.Name, 
+    c.Age, 
+    c.Breed, 
+    c.CoatColor, 
+    c.Sex, 
+    i.DateSurrendered
+    FROM Cats as c
+    LEFT JOIN Intake_Card i ON c.CatID = i.CatID
+    WHERE c.Adopted = 0; -- A value of 0 in adopted means the cat has not yet been adopted.
+-- This view shows important data from two different tables for only the cats that have not been adopted. 
+
+SELECT * from VisitorView
+
+CREATE VIEW CheckInventory AS 
+SELECT 
+    ProductID, ProductName, ProductType, Stock, MostRecentExpDate, CostPerUnit
+    FROM dbo.Inventory
+    WHERE Stock <= 2 OR MostRecentExpDate < GETDATE();
+-- This view checks the inventory table and only shows rows where stock is less than or equal to 3
+-- And the most recent experation date is in the past (expired).
+    
+Select * from CheckInventory
